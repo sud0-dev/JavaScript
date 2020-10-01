80f4449ed6acf0c82088cc8449b622125592a680
TheWrL0ck <66957901+TheWrL0ck@users.noreply.github.com>
2020-10-01 15:25:04 +0530

    Update script.js

--- Age in days/script.js
+++ Age in days/script.js
@@ -1,8 +1,8 @@
 function ageInDays () {
     var birthYear = prompt('Date of Birth: ');
-    var age = (2022 - birthYear) * 365
+    var age = (2020 - birthYear) * 365
     var h1 = document.createElement('h1');
-    var ans = document.createTextNode('Your age : ' + age + 'days');
+    var ans = document.createTextNode('Your age is : ' + age + 'days');
     h1.setAttribute('id', 'age');
     h1.appendChild(ans);
     document.getElementById('flex-box-result').appendChild(h1);
