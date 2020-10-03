5309233bac328630bc98f862f887e611986c8678
apoorva-rc <72261335+apoorva-rc@users.noreply.github.com>
2020-10-03 13:57:23 +0530

    Update script.js
    Added age description according to date and month of birth as was given previously according to year only

--- Age in days/script.js
+++ Age in days/script.js
@@ -1,6 +1,48 @@
 function ageInDays () {
-    var birthYear = prompt('Date of Birth: ');
-    var age = (2020 - birthYear) * 365
+    var birthYear = prompt('Year of Birth: ');
+    var birthMonth = promt ('Month of Birth');
+    var birthDate = prompt ('Date of Birth');
+    var d = new Date();
+    var day = d.getDate();
+    var mon = d.getMonth();
+    var age = (2020 - birthYear) * 365;
+    if (mon < birthMonth)
+    {
+        if( day == birthDate)
+        {
+            age = age+ (((12+mon) - birthMonth)*30);
+        }
+        else if (day > birthDate)
+        {
+            age = age+ (((12+mon) - birthMonth)*30)+(day-birthDate);
+        }
+        else 
+        {
+            age = age+(((12+(mon-1))-birthMonth)*30) +day ;
+        }
+    }
+    else
+    {
+        if( day == birthDate)
+        {
+            age = age+ ((mon-birthMonth)*30);
+        }
+        else if (day > birthDate)
+        {
+            age = age+ ((mon-birthMonth)*30))+(day-birthDate);
+        }
+        else 
+        {   
+            if( mon == birthMonth)
+            {
+                 age = age + ((30+day)-birthDate) ;
+            }
+            
+            else 
+            { 
+                age= age + ((mon-birthMonth)*30)+((30+day)-birthDate);
+            }
+        }
     var h1 = document.createElement('h1');
     var ans = document.createTextNode('Your age is : ' + age + 'days');
     h1.setAttribute('id', 'age');
